40365654ad426f952a295529947329d78bc14c61
srsh-kh <sur.khosh@gmail.com>
2022-12-31 11:26:12 +0330

    x

--- client/script.js
+++ client/script.js
@@ -1,19 +1,58 @@
-import bot from './assets/bog.svg';
-import user from './assets/user.svg';
+import bot from "./assets/bog.svg";
+import user from "./assets/user.svg";
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
 
 let loadInterval;
 
-function loader (element) {
-    element.textContent='';
+function loader(element) {
+  element.textContent = "";
 
-    loadInterval = setInterval(() => {
-        element.textContent += '.';
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
 
-        if (element.textContent === '....') {
-            element.textContent === '';
-        }
-    }, 3000)
+    if (element.textContent === "....") {
+      element.textContent === "";
+    }
+  }, 3000);
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innterHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+        <div class="wrapper ${isAi && "ai"}">
+            <div class ="chat">
+                <div className"profile">
+                    <img
+                    src="${isAi ? bot : user}"
+                    alt="${isAi ? "bot" : "user"}"
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        <div>
+    `
+  )
 }
